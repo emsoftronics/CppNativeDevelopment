d3ab5372d386a2e8f79b91a1cc36f1e097cf0fe7
Jagdish Prajapati <jagdish206prajapati@gmail.com>
2018-04-04 23:58:18 +0530

    testcases.cpp is added

---
+++ testcases.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testcases.cpp
+ *
+ *    Description:  This file has available all classes testcases which are
+ *                  available in src directory.
+ *
+ *        Version:  1.0
+ *        Created:  Wednesday 04 April 2018 11:47:06  IST
+ *       Revision:  1.1
+ *       Compiler:  g++
+ *
+ *         Author:  Jagdish Prajapati (JP), prajapatijagdish@gmail.com
+ *        Company:  Emsoftronic
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+   cout<< "\tTest cases are starting...\n";
+
+   cout<< "\tTest cases Finished!!\n";
+   return 0;
+}
